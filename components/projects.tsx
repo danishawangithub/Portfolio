1fa6b3f8e41df95b47c29c098030ec9e13cea10c
Rashid <142382111+danishawangithub@users.noreply.github.com>
2026-06-12 11:53:16 +0500

    update code

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { Badge } from "@/components/ui/badge"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { ArrowUpRight, ShoppingCart, Ticket, Newspaper, BookOpen, Monitor, Warehouse } from "lucide-react"
+import { ArrowUpRight, ShoppingCart, Ticket, Newspaper, BookOpen, Monitor, Warehouse,Bot } from "lucide-react"
 
 const projects = [
   {
